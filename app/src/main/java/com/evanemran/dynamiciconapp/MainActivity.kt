3374d0728e24f8ebf6439750368b107c5ee0e72f
Emran Khandaker Evan <evanemran007@gmail.com>
2024-02-12 11:59:03 +0600

    static fix

--- app/src/main/java/com/evanemran/dynamiciconapp/MainActivity.kt
+++ app/src/main/java/com/evanemran/dynamiciconapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.evanemran.dynamiciconapp
 
+import android.content.ComponentName
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.evanemran.dynamiciconapp.databinding.ActivityMainBinding
+
 
 public lateinit var aliasManager: KeyManager
 class MainActivity : AppCompatActivity() {
@@ -23,21 +27,71 @@ class MainActivity : AppCompatActivity() {
             binding.textViewAlias.text = it
         }
 
+//        val pm: PackageManager = packageManager
+//        pm.setComponentEnabledSetting(
+//            ComponentName(this, getString(R.string.alias_1)),
+//            PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+//        ) // Disable one alias
+
 
         binding.buttonOne.setOnClickListener {
             aliasManager.clearApiKey()
-            val iconChangeIntent = Intent("com.evanemran.dynamiciconapp.ACTION_CHANGE_ICON")
-            sendBroadcast(iconChangeIntent)
+//            val intentFilter: IntentFilter = IntentFilter()
+//            intentFilter.addAction("com.evanemran.dynamiciconapp.ACTION_CHANGE_ICON")
+//            val iconChangeIntent = Intent("com.evanemran.dynamiciconapp.ACTION_CHANGE_ICON")
+//            sendBroadcast(iconChangeIntent)
             aliasManager.selectedAlias = getString(R.string.alias_1)
+            setIconOne()
             Toast.makeText(this, getString(R.string.alias_1), Toast.LENGTH_SHORT).show()
         }
 
         binding.buttonTwo.setOnClickListener {
             aliasManager.clearApiKey()
-            val iconChangeIntent = Intent("com.evanemran.dynamiciconapp.ACTION_CHANGE_ICON")
-            sendBroadcast(iconChangeIntent)
+//            val iconChangeIntent = Intent("com.evanemran.dynamiciconapp.ACTION_CHANGE_ICON")
+//            sendBroadcast(iconChangeIntent)
             aliasManager.selectedAlias = getString(R.string.alias_2)
+            setIconTwo()
             Toast.makeText(this, getString(R.string.alias_2), Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun setIconOne() {
+        val pm = packageManager
+        pm.setComponentEnabledSetting(
+            ComponentName(
+                this,
+                "com.evanemran.dynamiciconapp.MainActivityAlias"
+            ),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+        pm.setComponentEnabledSetting(
+            ComponentName(
+                this,
+                "com.evanemran.dynamiciconapp.MainActivityAliasTwo"
+            ),
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }
+
+    private fun setIconTwo() {
+        val pm = packageManager
+        pm.setComponentEnabledSetting(
+            ComponentName(
+                this,
+                "com.evanemran.dynamiciconapp.MainActivityAliasTwo"
+            ),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+        pm.setComponentEnabledSetting(
+            ComponentName(
+                this,
+                "com.evanemran.dynamiciconapp.MainActivityAlias"
+            ),
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
     }
 }
